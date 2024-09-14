b8a0a6806ea09117f1e5bc873060ba3fa695c2e7
ajayee001 <mattewajayi001@gmail.com>
2024-09-14 00:43:09 +0000

    ajayi

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "ajayi"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "jayi"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "usdt"
 )
 
 var ID ids.ID
